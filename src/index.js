1097bc65e4e01c58bcf7b218cc7df4361a68e450
gbk <ck0123456@gmail.com>
2016-12-02 15:30:46 +0800

    fix includes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 * @Author: gbk <ck0123456@gmail.com>
 * @Date:   2016-04-21 17:34:00
 * @Last Modified by:   gbk
-* @Last Modified time: 2016-12-02 15:04:56
+* @Last Modified time: 2016-12-02 15:29:59
 */
 
 'use strict';
@@ -130,7 +130,7 @@ module.exports = {
             {
               test: /\.jsx?$/,
               loader: 'babel',
-              includes: includes,
+              include: includes,
               query: {
                 plugins: util.babel('plugin', [
                   'add-module-exports',
